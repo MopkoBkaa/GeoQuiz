25afa43cdbfbc0fe140fa5e952b2b8e4ff592643
MopkoBkaa <klimentyv56@gmail.com>
2025-08-08 19:28:33 +0300

    add button before and actions "next questions" before click true/false

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var false_button : Button
     private lateinit var next_button : Button
     private lateinit var question_text : TextView
+    private lateinit var before_button : Button
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -39,29 +40,24 @@ class MainActivity : AppCompatActivity() {
         false_button = findViewById(R.id.false_button)
         next_button = findViewById(R.id.next_button)
         question_text = findViewById(R.id.question_text)
+        before_button = findViewById(R.id.before_button)
 
         val questionTextResId = questionBank [currentIndex].textResId
         question_text.setText(questionTextResId)
 
         true_button.setOnClickListener{ view: View ->
             checkAnswer(true)
+            nextAnswer()
         }
-
         false_button.setOnClickListener{view: View ->
             checkAnswer(false)
+            nextAnswer()
         }
-
         next_button.setOnClickListener {
-
-            if (currentIndex + 1 < questionBank.size){
-                currentIndex++
-                updateQuestion()
-            }
-            if (currentIndex + 1 >= questionBank.size){
-                Toast.makeText(this,
-                    R.string.finish,
-                    Toast.LENGTH_SHORT).show()
-            }
+            nextAnswer()
+        }
+        before_button.setOnClickListener {
+            beforeAnswer()
         }
 
 
@@ -79,9 +75,34 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this,messageResId, Toast.LENGTH_SHORT).show()
     }
-
     private fun updateQuestion(){
         val questionTextResId = questionBank [currentIndex].textResId
         question_text.setText(questionTextResId)
+    }
+    private fun nextAnswer(){
+        if (currentIndex + 1 < questionBank.size){
+            currentIndex++
+            updateQuestion()
+        }
+        if (currentIndex + 1 >= questionBank.size){
+            Toast.makeText(this,
+                R.string.finish,
+                Toast.LENGTH_SHORT).show()
+        }
+        Log.i("index","$currentIndex , ${questionBank.size}")
+    }
+    private fun beforeAnswer(){
+        if (currentIndex > 0){
+            currentIndex --
+            updateQuestion()
+            Log.i("-index", "$currentIndex , ${questionBank.size}")
+        }
+        if (currentIndex == 0){
+            Log.i("index", "$currentIndex , ${questionBank.size}")
+            Toast.makeText(this,
+                R.string.first_question,
+                Toast.LENGTH_SHORT).show()
+        }
+        Log.i("index","$currentIndex , ${questionBank.size}")
     }
     }
